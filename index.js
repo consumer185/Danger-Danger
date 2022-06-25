eb00af66cb688ed1543691cac3b773e0c4802780
consumer185 <97405992+consumer185@users.noreply.github.com>
2022-06-25 22:35:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,32 @@
 // 1. play the next sequencial audio clip
 // 2. generate a random number between 1 and 21, use this to display *.svg image with random position and rotation
 
+var nextTrack = 0;
+var numberOfTracks = 2;
 
 
 window.addEventListener('touchstart', function() {
-  alert("the user touched the screen!!");
+  console.log(nextTrack);
+  audioPlay();
 });
+
+
+window.addEventListener('click', function() {
+  // alert("the user touched the screen!!");
+  console.log(nextTrack);
+  audioPlay();
+});
+
+
+function randomImageNumber() {
+
+}
+
+
+function audioPlay() {
+  if (nextTrack === numberOfTracks) {
+    nextTrack = 0;
+  } else if (nextTrack < numberOfTracks) {
+    nextTrack = nextTrack + 1;
+  }
+}
