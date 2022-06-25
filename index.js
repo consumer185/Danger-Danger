dd8c301c48dd4913e34ac1da5ec8262df858eadf
consumer185 <97405992+consumer185@users.noreply.github.com>
2022-06-25 22:55:11 +0100

    random image logic

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
 // on click/tap
 // 1. play the next sequencial audio clip
-// 2. generate a random number between 1 and 21, use this to display *.svg image with random position and rotation
+// 2. generate a random number between 0 and 20, use this to display *.svg image with random position and rotation
 
 var nextTrack = 0;
 var numberOfTracks = 2;
+var numberOfImages = 20;
 
 
 window.addEventListener('touchstart', function() {
-  console.log(nextTrack);
   audioPlay();
+  imageDisplay();
+  console.log("now playing the following audio: " + nextTrack);
+  console.log("now displaying the following image: " + imageToDisplay);
 });
 
 
 window.addEventListener('click', function() {
-  // alert("the user touched the screen!!");
-  console.log(nextTrack);
   audioPlay();
+  imageDisplay();
+  console.log("now playing the following audio: " + nextTrack);
+  console.log("now displaying the following image: " + imageToDisplay);
 });
 
 
-function randomImageNumber() {
+function imageDisplay() {
+  randomNumber = Math.floor(Math.random() * 21);
+imageToDisplay = randomNumber + ".svg";
+
 
 }
 
 
 function audioPlay() {
+
   if (nextTrack === numberOfTracks) {
     nextTrack = 0;
   } else if (nextTrack < numberOfTracks) {
