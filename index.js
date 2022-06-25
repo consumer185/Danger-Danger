9ab9ce1251e27fe3277fb82d80727d991329b786
consumer185 <97405992+consumer185@users.noreply.github.com>
2022-06-25 22:19:24 +0100

    Update index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+// on click/tap, select a random number between 1 and 21 inclusive, use this to select an image *.svg to display in a random position, rotation, vertical
+
+window.addEventListener('touchstart', function() {
+  alert("the user touched the screen!!");
+});
